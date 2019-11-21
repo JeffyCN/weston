2db20b102716cd9c1e3523bd04f987bc9ce1a933
Pekka Paalanen <pekka.paalanen@collabora.com>
2019-11-21 15:04:28 +0200

    gitlab-ci: wrap and alphabetize apt-get line
    This line is far too long, it makes it really had to see in a diff what
    changed. Wrap it into multiple lines and sort them alphabetically. If a piece
    of it changes in the future, it will be easy to see in a diff.
    
    Signed-off-by: Pekka Paalanen <pekka.paalanen@collabora.com>

--- .gitlab-ci/debian-install.sh
+++ .gitlab-ci/debian-install.sh
@@ -4,7 +4,59 @@ set -o xtrace -o errexit
 
 echo 'deb http://deb.debian.org/debian buster-backports main' >> /etc/apt/sources.list
 apt-get update
-apt-get -y --no-install-recommends install build-essential automake autoconf libtool pkg-config libexpat1-dev libffi-dev libxml2-dev libpixman-1-dev libpng-dev libjpeg-dev libcolord-dev mesa-common-dev libglu1-mesa-dev libegl1-mesa-dev libgles2-mesa-dev libwayland-dev libxcb1-dev libxcb-composite0-dev libxcb-xfixes0-dev libxcb-xkb-dev libx11-xcb-dev libx11-dev libudev-dev libgbm-dev libxkbcommon-dev libcairo2-dev libpango1.0-dev libgdk-pixbuf2.0-dev libxcursor-dev libmtdev-dev libpam0g-dev libvpx-dev libsystemd-dev libevdev-dev libinput-dev libwebp-dev libjpeg-dev libva-dev liblcms2-dev git libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev freerdp2-dev curl python3-pip python3-setuptools doxygen ninja-build libdbus-1-dev libpipewire-0.2-dev
+apt-get -y --no-install-recommends install \
+	autoconf \
+	automake \
+	build-essential \
+	curl \
+	doxygen \
+	freerdp2-dev \
+	git \
+	libcairo2-dev \
+	libcolord-dev \
+	libdbus-1-dev \
+	libegl1-mesa-dev \
+	libevdev-dev \
+	libexpat1-dev \
+	libffi-dev \
+	libgbm-dev \
+	libgdk-pixbuf2.0-dev \
+	libgles2-mesa-dev \
+	libglu1-mesa-dev \
+	libgstreamer1.0-dev \
+	libgstreamer-plugins-base1.0-dev \
+	libinput-dev \
+	libjpeg-dev \
+	libjpeg-dev \
+	liblcms2-dev \
+	libmtdev-dev \
+	libpam0g-dev \
+	libpango1.0-dev \
+	libpipewire-0.2-dev \
+	libpixman-1-dev \
+	libpng-dev \
+	libsystemd-dev \
+	libtool \
+	libudev-dev \
+	libva-dev \
+	libvpx-dev \
+	libwayland-dev \
+	libwebp-dev \
+	libx11-dev \
+	libx11-xcb-dev \
+	libxcb1-dev \
+	libxcb-composite0-dev \
+	libxcb-xfixes0-dev \
+	libxcb-xkb-dev \
+	libxcursor-dev \
+	libxkbcommon-dev \
+	libxml2-dev \
+	mesa-common-dev \
+	ninja-build \
+	pkg-config \
+	python3-pip \
+	python3-setuptools \
+
 
 pip3 install --user git+https://github.com/mesonbuild/meson.git@0.49
 # for documentation
