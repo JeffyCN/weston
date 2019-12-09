a8e93ed90f68d8a13649a49527feb026e22bfdfa
Marco Felsch <m.felsch@pengutronix.de>
2019-12-09 12:30:13 +0100

    ivi-shell: add screenshot capability
    Various shells provide the functionality to take screenshots using the
    weston-screenshooter or a key combination. This is useful on the ivi-shell, too.
    
    Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>
    Signed-off-by: Michael Tretter <m.tretter@pengutronix.de>

--- ivi-shell/ivi-shell.c
+++ ivi-shell/ivi-shell.c
@@ -722,6 +722,9 @@ wet_shell_init(struct weston_compositor *compositor,
 		goto err_desktop;
 
 	ivi_layout_init_with_compositor(compositor);
+
+	screenshooter_create(compositor);
+
 	shell_add_bindings(compositor, shell);
 
 	return IVI_SUCCEEDED;
