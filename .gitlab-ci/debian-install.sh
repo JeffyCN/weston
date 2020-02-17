8bd46c75f9362774e83f53d934b24b170d021f04
Pekka Paalanen <pekka.paalanen@collabora.com>
2020-02-17 15:32:18 +0200

    CI: build a Mesa shapshot for getImage/putImage on pbuffer swrast
    https://gitlab.freedesktop.org/mesa/mesa/commit/c7617d8908a970124321ce731b43d5996c3c5775
    is necessary for running GL-renderer with llvmpipe in Gitlab CI.
    
    Signed-off-by: Pekka Paalanen <pekka.paalanen@collabora.com>

--- .gitlab-ci/debian-install.sh
+++ .gitlab-ci/debian-install.sh
@@ -2,6 +2,24 @@
 
 set -o xtrace -o errexit
 
+# These get temporary installed for building Mesa and then force-removed.
+MESA_DEV_PKGS="
+	bison
+	flex
+	gettext
+	libwayland-egl-backend-dev
+	libxrandr-dev
+	llvm-8-dev
+	python-mako
+	python3-mako
+	wayland-protocols
+"
+
+# Needed for running the custom-built mesa
+MESA_RUNTIME_PKGS="
+	libllvm8
+"
+
 echo 'deb http://deb.debian.org/debian buster-backports main' >> /etc/apt/sources.list
 apt-get update
 apt-get -y --no-install-recommends install \
@@ -57,9 +75,11 @@ apt-get -y --no-install-recommends install \
 	python3-pip \
 	python3-setuptools \
 	xwayland \
+	$MESA_RUNTIME_PKGS
 
 
 pip3 install --user git+https://github.com/mesonbuild/meson.git@0.49
+export PATH=$HOME/.local/bin:$PATH
 # for documentation
 pip3 install sphinx==2.1.0 --user
 pip3 install breathe==4.13.0.post0 --user
@@ -74,6 +94,17 @@ cd build
 ../autogen.sh --disable-documentation
 make install
 cd ../../
+
+apt-get -y --no-install-recommends install $MESA_DEV_PKGS
+git clone --single-branch --branch master --shallow-since='2020-02-15' https://gitlab.freedesktop.org/mesa/mesa.git mesa
+cd mesa
+git checkout -b snapshot c7617d8908a970124321ce731b43d5996c3c5775
+meson build -Dauto_features=disabled \
+	-Dgallium-drivers=swrast -Dvulkan-drivers= -Ddri-drivers=
+ninja -C build install
+cd ..
+rm -rf mesa
+apt-get -y --autoremove purge $MESA_DEV_PKGS
 
 mkdir -p /tmp/.X11-unix
 chmod 777 /tmp/.X11-unix
