a41f41b8e4339fb8ae60d6bc6ee344f9dc1bd11f
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-06-23 10:05:48 +0800

    backend-drm: Support selecting monitors
    Using these environments:
    
    WESTON_DRM_PRIMARY:
      Specify primary connector.
    WESTON_DRM_SINGLE_HEAD:
      Force using single connector.
    WESTON_DRM_HEAD_FALLBACK:
      Fallback to any available connector if none matched.
    WESTON_DRM_HEAD_FALLBACK_ALL:
      Fallback to all available connector if none matched.
    WESTON_DRM_PREFER_EXTERNAL_DUAL:
      Prefer external connectors, and also enable internal ones.
    WESTON_DRM_PREFER_EXTERNAL:
      Prefer external connectors, and disable internal ones if any matched.
    
    WESTON_DRM_HEAD_MODE:
      default(match primary->internal->external)
      primary(match primary only)
      internal(match primary->internal)
      external(match primary->external)
      external-dual(match primary->external->internal)
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- desktop-shell/shell.c
+++ desktop-shell/shell.c
@@ -2147,7 +2147,7 @@ shell_configure_fullscreen(struct shell_surface *shsurf)
 	                                &surf_width, &surf_height);
 
 	if (surface->buffer_ref.buffer)
-		center_on_output(shsurf->view, shsurf->fullscreen_output);
+		center_on_output(shsurf->view, shsurf->output);
 }
 
 static void
@@ -2454,7 +2454,7 @@ map(struct desktop_shell *shell, struct shell_surface *shsurf,
 
 	/* initial positioning, see also configure() */
 	if (shsurf->state.fullscreen) {
-		center_on_output(shsurf->view, shsurf->fullscreen_output);
+		center_on_output(shsurf->view, shsurf->output);
 		shell_map_fullscreen(shsurf);
 	} else if (shsurf->state.maximized) {
 		set_maximized_position(shell, shsurf);
@@ -3988,6 +3988,9 @@ shell_fade_done_for_output(struct weston_view_animation *animation, void *data)
 	struct shell_output *shell_output = data;
 	struct desktop_shell *shell = shell_output->shell;
 
+	if (!shell_output->fade.view)
+		return;
+
 	shell_output->fade.animation = NULL;
 	switch (shell_output->fade.type) {
 	case FADE_IN:
@@ -4021,6 +4024,7 @@ shell_fade_create_surface_for_output(struct desktop_shell *shell, struct shell_o
 
 	weston_surface_set_size(surface, shell_output->output->width, shell_output->output->height);
 	weston_view_set_position(view, shell_output->output->x, shell_output->output->y);
+	weston_view_set_output(view, shell_output->output);
 	weston_surface_set_color(surface, 0.0, 0.0, 0.0, 1.0);
 	weston_layer_entry_insert(&compositor->fade_layer.view_list,
 				  &view->layer_link);
@@ -4795,9 +4799,8 @@ shell_reposition_view_on_output_change(struct weston_view *view)
 	if (!shsurf)
 		return;
 
-	shsurf->saved_position_valid = false;
-	set_maximized(shsurf, false);
-	set_fullscreen(shsurf, false, NULL);
+	if (shsurf->fullscreen_output && shsurf->fullscreen_output->destroying)
+		shsurf->fullscreen_output = NULL;
 }
 
 void
@@ -4841,8 +4844,11 @@ shell_output_destroy(struct shell_output *shell_output)
 	}
 
 	if (shell_output->fade.view) {
+		struct weston_view *view = shell_output->fade.view;
+		shell_output->fade.view = NULL;
+
 		/* destroys the view as well */
-		weston_surface_destroy(shell_output->fade.view->surface);
+		weston_surface_destroy(view->surface);
 	}
 
 	if (shell_output->fade.startup_timer)
@@ -4946,12 +4952,24 @@ handle_output_move_layer(struct desktop_shell *shell,
 static void
 handle_output_move(struct wl_listener *listener, void *data)
 {
+	struct weston_output *output = data;
+	struct weston_compositor *compositor = output->compositor;
 	struct desktop_shell *shell;
 
 	shell = container_of(listener, struct desktop_shell,
 			     output_move_listener);
 
-	shell_for_each_layer(shell, handle_output_move_layer, data);
+	if (shell->lock_surface)
+		shell->lock_surface->committed(shell->lock_surface, 0, 0);
+
+	/* Only move normal layers for non-default output */
+	if (output != get_default_output(compositor)) {
+		shell_for_each_layer(shell, handle_output_move_layer, data);
+	} else {
+		handle_output_move_layer(shell, &shell->lock_layer, data);
+		handle_output_move_layer(shell, &shell->background_layer, data);
+		handle_output_move_layer(shell, &shell->panel_layer, data);
+	}
 }
 
 static void
