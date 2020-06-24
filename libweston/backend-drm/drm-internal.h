3490660280ebe47df105cbfd34e3ae7de6f6f2e3
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-06-24 11:59:42 +0800

    backend-drm: Support virtual screen size
    Support setting virtual screen size, for example:
    export WESTON_DRM_VIRTUAL_SIZE=1024x768
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libweston/backend-drm/drm-internal.h
+++ libweston/backend-drm/drm-internal.h
@@ -166,6 +166,7 @@ enum wdrm_plane_property {
 	WDRM_PLANE_IN_FENCE_FD,
 	WDRM_PLANE_FB_DAMAGE_CLIPS,
 	WDRM_PLANE_ZPOS,
+	WDRM_PLANE_FEATURE,
 	WDRM_PLANE__COUNT
 };
 
@@ -177,6 +178,15 @@ enum wdrm_plane_type {
 	WDRM_PLANE_TYPE_CURSOR,
 	WDRM_PLANE_TYPE_OVERLAY,
 	WDRM_PLANE_TYPE__COUNT
+};
+
+/**
+ * Possible values for the WDRM_PLANE_FEATURE property.
+ */
+enum wdrm_plane_feature {
+	WDRM_PLANE_FEATURE_SCALE = 0,
+	WDRM_PLANE_FEATURE_ALPHA,
+	WDRM_PLANE_FEATURE__COUNT
 };
 
 /**
@@ -325,6 +335,7 @@ struct drm_backend {
 	bool pending_update;
 	int64_t last_update_ms;
 	int64_t last_resize_ms;
+	int64_t resize_freeze_ms;
 
 	bool single_head;
 	bool head_fallback;
@@ -332,6 +343,9 @@ struct drm_backend {
 	drm_head_match_t *head_matches;
 	struct drm_head *primary_head;
 	struct wl_listener output_create_listener;
+
+	int virtual_width;
+	int virtual_height;
 };
 
 struct drm_mode {
@@ -501,6 +515,8 @@ struct drm_plane {
 	struct wl_list link;
 
 	struct weston_drm_format_array formats;
+
+	bool can_scale;
 };
 
 struct drm_connector {
@@ -599,6 +615,9 @@ struct drm_output {
 	submit_frame_cb virtual_submit_frame;
 
 	bool state_invalid;
+
+	/* The dummy framebuffer for SET_CRTC. */
+	struct drm_fb *fb_dummy;
 };
 
 static inline struct drm_head *
@@ -696,6 +715,10 @@ uint64_t
 drm_property_get_value(struct drm_property_info *info,
 		       const drmModeObjectProperties *props,
 		       uint64_t def);
+bool
+drm_property_has_feature(struct drm_property_info *infos,
+			 const drmModeObjectProperties *props,
+			 enum wdrm_plane_feature feature);
 uint64_t *
 drm_property_get_range_values(struct drm_property_info *info,
 			      const drmModeObjectProperties *props);
--- libweston/backend-drm/drm.c
+++ libweston/backend-drm/drm.c
@@ -329,6 +329,11 @@ drm_output_update_complete(struct drm_output *output, uint32_t flags,
 		output->state_last = NULL;
 	}
 
+	if (output->fb_dummy) {
+		drm_fb_unref(output->fb_dummy);
+		output->fb_dummy = NULL;
+	}
+
 	if (output->destroy_pending) {
 		output->destroy_pending = false;
 		output->disable_pending = false;
@@ -402,6 +407,7 @@ drm_output_render(struct drm_output_state *state, pixman_region32_t *damage)
 	struct drm_property_info *damage_info =
 		&scanout_plane->props[WDRM_PLANE_FB_DAMAGE_CLIPS];
 	struct drm_backend *b = to_drm_backend(c);
+	struct drm_mode *mode;
 	struct drm_fb *fb;
 	pixman_region32_t scanout_damage;
 	pixman_box32_t *rects;
@@ -445,10 +451,11 @@ drm_output_render(struct drm_output_state *state, pixman_region32_t *damage)
 	scanout_state->src_w = fb->width << 16;
 	scanout_state->src_h = fb->height << 16;
 
+	mode = to_drm_mode(output->base.current_mode);
 	scanout_state->dest_x = 0;
 	scanout_state->dest_y = 0;
-	scanout_state->dest_w = output->base.current_mode->width;
-	scanout_state->dest_h = output->base.current_mode->height;
+	scanout_state->dest_w = mode->mode_info.hdisplay;
+	scanout_state->dest_h = mode->mode_info.vdisplay;
 
 	pixman_region32_subtract(&c->primary_plane.damage,
 				 &c->primary_plane.damage, damage);
@@ -521,7 +528,7 @@ drm_output_repaint(struct weston_output *output_base,
 
 	weston_compositor_read_presentation_clock(b->compositor, &now);
 	now_ms = timespec_to_msec(&now);
-	if (now_ms < b->last_resize_ms + DRM_RESIZE_FREEZE_MS) {
+	if (now_ms < b->last_resize_ms + b->resize_freeze_ms) {
 		/* Resize fullscreen/maxmium views(not always success) */
 		if (now_ms < b->last_resize_ms + DRM_RESIZE_FREEZE_MS)
 			wl_signal_emit(&b->compositor->output_resized_signal,
@@ -856,6 +863,11 @@ drm_plane_create(struct drm_backend *b, const drmModePlane *kplane)
 		drm_property_get_value(&plane->props[WDRM_PLANE_TYPE],
 				       props,
 				       WDRM_PLANE_TYPE__COUNT);
+
+	plane->can_scale =
+		drm_property_has_feature(plane->props,
+					 props,
+					 WDRM_PLANE_FEATURE_SCALE);
 
 	zpos_range_values =
 		drm_property_get_range_values(&plane->props[WDRM_PLANE_ZPOS],
@@ -3300,6 +3312,16 @@ drm_backend_create(struct weston_compositor *compositor,
 	}
 
 	b->head_matches = drm_head_matches[head_mode];
+
+	buf = getenv("WESTON_DRM_VIRTUAL_SIZE");
+	if (buf)
+		sscanf(buf, "%dx%d", &b->virtual_width, &b->virtual_height);
+
+	buf = getenv("WESTON_DRM_RESIZE_FREEZE_MS");
+	if (buf)
+		b->resize_freeze_ms = atoi(buf);
+	else
+		b->resize_freeze_ms = DRM_RESIZE_FREEZE_MS;
 
 	b->state_invalid = true;
 	b->drm.fd = -1;
