ca87415edf78811a3e7ea1569780f68c7b5657f6
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-07-03 14:23:45 +0800

    pixman-renderer: Support yuv formats
    Support yuy2/yv12/i420/nv12/nv16.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libweston/pixel-formats.c
+++ libweston/pixel-formats.c
@@ -459,6 +459,7 @@ static const struct pixel_format_info pixel_format_table[] = {
 		SAMPLER_TYPE(EGL_TEXTURE_Y_XUXV_WL),
 		.num_planes = 1,
 		.hsub = 2,
+		PIXMAN_FMT(yuy2),
 	},
 	{
 		DRM_FORMAT(YVYU),
@@ -488,6 +489,9 @@ static const struct pixel_format_info pixel_format_table[] = {
 		.num_planes = 2,
 		.hsub = 2,
 		.vsub = 2,
+#ifdef HAVE_PIXMAN_NV12
+		PIXMAN_FMT(nv12),
+#endif
 	},
 	{
 		DRM_FORMAT(NV21),
@@ -503,6 +507,9 @@ static const struct pixel_format_info pixel_format_table[] = {
 		.num_planes = 2,
 		.hsub = 2,
 		.vsub = 1,
+#ifdef HAVE_PIXMAN_NV16
+		PIXMAN_FMT(nv16),
+#endif
 	},
 	{
 		DRM_FORMAT(NV61),
@@ -559,6 +566,9 @@ static const struct pixel_format_info pixel_format_table[] = {
 		.num_planes = 3,
 		.hsub = 2,
 		.vsub = 2,
+#ifdef HAVE_PIXMAN_I420
+		PIXMAN_FMT(i420),
+#endif
 	},
 	{
 		DRM_FORMAT(YVU420),
@@ -567,6 +577,7 @@ static const struct pixel_format_info pixel_format_table[] = {
 		.chroma_order = ORDER_VU,
 		.hsub = 2,
 		.vsub = 2,
+		PIXMAN_FMT(yv12),
 	},
 	{
 		DRM_FORMAT(YUV422),
