dae77a914a8c03b41cf72fbf04882560a326a4d3
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-07-03 12:37:37 +0800

    backend-drm: Support controlling output dynamically
    Use config file to set output's rotate/down-scale/size/pos/mode/off/
    freeze/input/display-rectangle and prefer/primary output.
    
    Default config file is "/tmp/.weston_drm.conf", can override with
    "WESTON_DRM_CONFIG" environment.
    
    Supported configs format is "output:<output name>:<config>", for
    example:
    echo "output:DSI-1:state=off" >> /tmp/.weston_drm.conf
    echo "output:DSI-1:state=on" >> /tmp/.weston_drm.conf
    echo "output:DSI-1:state=detect" >> /tmp/.weston_drm.conf
    echo "output:DSI-1:off" >> /tmp/.weston_drm.conf
    echo "output:eDP-1:freeze" >> /tmp/.weston_drm.conf
    echo "output:DSI-1:offscreen" >> /tmp/.weston_drm.conf
    echo "output:all:rotate90" >> /tmp/.weston_drm.conf
    echo "output:all:rect=<100,20,1636,2068>" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:mode=800x600" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:pos=100,200" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:size=1920x1080" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:prefer" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:primary" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:down-scale=0.5" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:input=*" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:input=" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:input=event6" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:input=goodix*" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:input=goodix-ts" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:refresh" >> /tmp/.weston_drm.conf
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libweston/backend-drm/drm-internal.h
+++ libweston/backend-drm/drm-internal.h
@@ -119,6 +119,9 @@
 /* Min duration between drm outputs update requests, to avoid glith */
 #define DRM_MIN_UPDATE_MS	1000
 
+#define WESTON_DRM_CONFIG_FILE	"/tmp/.weston_drm.conf"
+#define DRM_CONFIG_UPDATE_MS	100
+
 /**
  * Represents the values of an enum-type KMS property
  */
@@ -272,7 +275,6 @@ struct drm_backend {
 	struct wl_event_source *hotplug_timer;
 	bool pending_update;
 	int64_t last_update_ms;
-	int64_t resize_freeze_ms;
 
 	bool single_head;
 	bool head_fallback;
@@ -285,6 +287,9 @@ struct drm_backend {
 	int virtual_height;
 
 	bool mirror_mode;
+
+	struct wl_event_source *config_timer;
+	struct stat config_stat;
 };
 
 struct drm_mode {
@@ -518,6 +523,12 @@ struct drm_writeback {
 	struct weston_drm_format_array formats;
 };
 
+enum drm_head_state {
+	DRM_HEAD_DETECT,
+	DRM_HEAD_OFF,
+	DRM_HEAD_ON,
+};
+
 struct drm_head {
 	struct weston_head base;
 	struct drm_connector connector;
@@ -533,6 +544,8 @@ struct drm_head {
 
 	void *display_data;             /**< EDID or DisplayID blob */
 	size_t display_data_len;        /**< bytes */
+
+	enum drm_head_state state;
 };
 
 struct drm_crtc {
@@ -627,7 +640,12 @@ struct drm_output {
 
 	bool is_mirror;
 
+	bool freezing;
+	bool offscreen;
+
 	pixman_box32_t plane_bounds;
+
+	uint32_t original_transform;
 };
 
 void
@@ -745,6 +763,13 @@ drm_mode_list_destroy(struct drm_device *device, struct wl_list *mode_list);
 
 void
 drm_output_print_modes(struct drm_output *output);
+
+struct drm_mode *
+drm_output_choose_initial_mode(struct drm_device *device,
+			       struct drm_output *output,
+			       enum weston_drm_backend_output_mode mode,
+			       const char *modeline,
+			       const drmModeModeInfo *current_mode);
 
 int
 drm_output_set_mode(struct weston_output *base,
@@ -963,3 +988,12 @@ drm_output_render_gl(struct drm_output_state *state, pixman_region32_t *damage)
 	return NULL;
 }
 #endif
+
+inline static bool
+drm_head_is_connected(struct drm_head *head)
+{
+	if (!head || !head->connector.conn)
+		return false;
+
+	return head->connector.conn->connection == DRM_MODE_CONNECTED;
+}
