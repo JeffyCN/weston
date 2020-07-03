2a6a3365dab47f777ad284dd3ae29d85444fda72
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-07-03 10:20:40 +0800

    compositor: Support holding display for the first app
    Add a warm-up option to hold display until the first desktop app appears.
    This avoids showing a blank screen during startup.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- frontend/main.c
+++ frontend/main.c
@@ -757,6 +757,7 @@ usage(int error_code)
 		"  -f, --flight-rec-scopes=SCOPE\n\t\t\tSpecify log scopes to "
 			"subscribe to.\n\t\t\tCan specify multiple scopes, "
 			"each followed by comma\n"
+		"  -w, --warm-up\t\tHold display for the first app\n"
 		"  -h, --help\t\tThis help message\n\n");
 
 #if defined(BUILD_DRM_COMPOSITOR)
@@ -5336,6 +5337,8 @@ wet_main(int argc, char *argv[], const struct weston_testsuite_data *test_data)
 	bool wait_for_debugger = false;
 	struct wl_protocol_logger *protologger = NULL;
 
+	bool warm_up = false;
+
 	const struct weston_option core_options[] = {
 		{ WESTON_OPTION_STRING, "backend", 'B', &backends },
 		{ WESTON_OPTION_STRING, "backends", 0, &backends },
@@ -5358,6 +5361,7 @@ wet_main(int argc, char *argv[], const struct weston_testsuite_data *test_data)
 		{ WESTON_OPTION_STRING, "debug-scopes", 'd', &debug_scopes },
 		{ WESTON_OPTION_STRING, "logger-scopes", 'l', &log_scopes },
 		{ WESTON_OPTION_STRING, "flight-rec-scopes", 'f', &flight_rec_scopes },
+		{ WESTON_OPTION_BOOLEAN, "warm-up", 'w', &warm_up },
 	};
 
 	wl_list_init(&wet.layoutput_list);
@@ -5598,9 +5602,13 @@ wet_main(int argc, char *argv[], const struct weston_testsuite_data *test_data)
 	if (idle_time < 0)
 		idle_time = 300; /* default idle timeout, in seconds */
 
+	if (!warm_up)
+		weston_config_section_get_bool(section, "warm-up", &warm_up, false);
+
 	wet.compositor->idle_time = idle_time;
 	wet.compositor->default_pointer_grab = NULL;
 	wet.compositor->exit = handle_exit;
+	wet.compositor->lazy_output_ready = warm_up;
 
 	weston_compositor_log_capabilities(wet.compositor);
 
--- include/libweston/libweston.h
+++ include/libweston/libweston.h
@@ -603,6 +603,9 @@ struct weston_output {
 
 	/* Needs full buffer clear */
 	bool full_clear_needed;
+
+	/* Output is ready but waiting for first desktop surface */
+	bool lazy_ready;
 };
 
 enum weston_pointer_motion_mask {
@@ -1602,6 +1605,9 @@ struct weston_compositor {
 	/* Primary output dimensions for mirrors */
 	int32_t mirror_width;
 	int32_t mirror_height;
+
+	/* Delay output display until first desktop surface (warm-up) */
+	bool lazy_output_ready;
 };
 
 struct weston_solid_buffer_values {
