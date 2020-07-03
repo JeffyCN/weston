6e7b6d13ed9901632e51c687d43b21f8a2807c6c
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-07-03 12:37:37 +0800

    backend-drm: Support controlling output dynamically
    Use config file to set output's rotate/down-scale/size/pos/mode/off/
    freeze/input/display-rectangle and prefer/primary output.
    
    Default config file is "/tmp/.weston_drm.conf", can override with
    "WESTON_DRM_CONFIG" environment.
    
    Supported configs format is "output:<output name>:<config>", for
    example:
    echo "output:DSI-1:state=off" >> /tmp/.weston_drm.conf
    echo "output:DSI-1:state=on" >> /tmp/.weston_drm.conf
    echo "output:DSI-1:state=detect" >> /tmp/.weston_drm.conf
    echo "output:DSI-1:off" >> /tmp/.weston_drm.conf
    echo "output:eDP-1:freeze" >> /tmp/.weston_drm.conf
    echo "output:DSI-1:offscreen" >> /tmp/.weston_drm.conf
    echo "output:all:rotate90" >> /tmp/.weston_drm.conf
    echo "output:all:rect=<100,20,1636,2068>" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:mode=800x600" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:pos=100,200" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:size=1920x1080" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:prefer" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:primary" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:down-scale=0.5" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:input=*" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:input=" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:input=event6" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:input=goodix*" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:input=goodix-ts" >> /tmp/.weston_drm.conf
    echo "output:HDMI-A-1:refresh" >> /tmp/.weston_drm.conf
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- desktop-shell/shell.c
+++ desktop-shell/shell.c
@@ -922,6 +922,12 @@ touch_move_grab_motion(struct weston_touch_grab *grab,
 	if (!shsurf || !shsurf->desktop_surface || !move->active)
 		return;
 
+	/* Ignore pinned output when grabbing. */
+	if (shsurf->view && shsurf->view->pinned_output) {
+		free(shsurf->view->pinned_output);
+		shsurf->view->pinned_output = NULL;
+	}
+
 	es = weston_desktop_surface_get_surface(shsurf->desktop_surface);
 
 	weston_view_set_position(shsurf->view, dx, dy);
@@ -1053,6 +1059,12 @@ move_grab_motion(struct weston_pointer_grab *grab,
 	weston_pointer_move(pointer, event);
 	if (!shsurf || !shsurf->desktop_surface)
 		return;
+
+	/* Ignore pinned output when grabbing. */
+	if (shsurf->view && shsurf->view->pinned_output) {
+		free(shsurf->view->pinned_output);
+		shsurf->view->pinned_output = NULL;
+	}
 
 	surface = weston_desktop_surface_get_surface(shsurf->desktop_surface);
 
@@ -1469,8 +1481,6 @@ shell_surface_set_output(struct shell_surface *shsurf,
 		shsurf->output = output;
 	else if (es->output)
 		shsurf->output = es->output;
-	else
-		shsurf->output = get_default_output(es->compositor);
 
 	if (shsurf->output_destroy_listener.notify) {
 		wl_list_remove(&shsurf->output_destroy_listener.link);
@@ -2176,6 +2186,13 @@ set_fullscreen(struct shell_surface *shsurf, bool fullscreen,
 		shell_surface_set_output(shsurf, output);
 		shsurf->fullscreen_output = shsurf->output;
 
+		/* Update fullscreen view's position */
+		if (output)
+			weston_view_set_position(shsurf->view,
+						 output->x, output->y);
+		else
+			weston_view_set_position(shsurf->view, 0, 0);
+
 		if (shsurf->output) {
 			width = shsurf->output->width;
 			height = shsurf->output->height;
@@ -2184,6 +2201,8 @@ set_fullscreen(struct shell_surface *shsurf, bool fullscreen,
 							WESTON_TOP_LEVEL_TILED_ORIENTATION_NONE);
 	} else if (weston_desktop_surface_get_maximized(desktop_surface)) {
 		get_maximized_size(shsurf, &width, &height);
+
+		set_maximized_position(shsurf->shell, shsurf);
 	}
 	weston_desktop_surface_set_fullscreen(desktop_surface, fullscreen);
 	weston_desktop_surface_set_size(desktop_surface, width, height);
@@ -2301,6 +2320,8 @@ set_maximized(struct shell_surface *shsurf, bool maximized)
 		shell_surface_set_output(shsurf, output);
 
 		get_maximized_size(shsurf, &width, &height);
+
+                set_maximized_position(shsurf->shell, shsurf);
 
 		weston_desktop_surface_set_orientation(shsurf->desktop_surface,
 							WESTON_TOP_LEVEL_TILED_ORIENTATION_NONE);
@@ -3832,6 +3853,7 @@ weston_view_set_initial_position(struct weston_view *view,
 	int32_t range_x, range_y;
 	int32_t x, y;
 	struct weston_output *output, *target_output = NULL;
+	struct weston_output *preferred_output = NULL;
 	struct weston_seat *seat;
 	pixman_rectangle32_t area;
 
@@ -3856,15 +3878,19 @@ weston_view_set_initial_position(struct weston_view *view,
 		}
 	}
 
-	wl_list_for_each(output, &compositor->output_list, link) {
+	wl_list_for_each_reverse(output, &compositor->output_list, link) {
 		if (!weston_output_valid(output))
 			continue;
 
-		if (pixman_region32_contains_point(&output->region, ix, iy, NULL)) {
+		if (weston_output_preferred(output))
+			preferred_output = output;
+
+		if (pixman_region32_contains_point(&output->region, ix, iy, NULL))
 			target_output = output;
-			break;
-		}
 	}
+
+	if (preferred_output)
+		target_output = preferred_output;
 
 	if (!target_output) {
 		weston_view_set_position(view, 10 + random() % 400,
@@ -4282,7 +4308,20 @@ shell_reposition_view_on_output_change(struct weston_view *view)
 		}
 	}
 
+	shsurf = get_shell_surface(view->surface);
+	if (!shsurf)
+		return;
+
+	if (!weston_output_valid(shsurf->fullscreen_output))
+		shsurf->fullscreen_output = NULL;
+
 	if (!visible) {
+		if (ec->pin_output && view->pinned_output)
+			return;
+
+		if (shsurf->state.fullscreen || shsurf->state.maximized)
+			return;
+
 		first_output = container_of(ec->output_list.next,
 					    struct weston_output, link);
 
@@ -4293,14 +4332,6 @@ shell_reposition_view_on_output_change(struct weston_view *view)
 	} else {
 		weston_view_geometry_dirty(view);
 	}
-
-
-	shsurf = get_shell_surface(view->surface);
-	if (!shsurf)
-		return;
-
-	if (!weston_output_valid(shsurf->fullscreen_output))
-		shsurf->fullscreen_output = NULL;
 }
 
 void
@@ -4381,6 +4412,41 @@ shell_resize_surface_to_output(struct desktop_shell *shell,
 					output->height);
 }
 
+static void
+handle_output_resize_layer(struct desktop_shell *shell,
+			   struct weston_layer *layer, void *data)
+{
+	struct weston_output *output = data;
+	struct weston_view *view;
+
+	wl_list_for_each(view, &layer->view_list.link, layer_link.link) {
+		struct weston_desktop_surface *desktop_surface;
+		struct shell_surface *shsurf;
+		bool dirty = false;
+
+		if (view->output != output)
+			continue;
+
+		shsurf = get_shell_surface(view->surface);
+		if (!shsurf)
+			continue;
+
+		desktop_surface = shsurf->desktop_surface;
+		if (weston_desktop_surface_get_fullscreen(desktop_surface)) {
+			set_fullscreen(shsurf, true, output);
+			dirty = true;
+		}
+		if (weston_desktop_surface_get_maximized(desktop_surface)) {
+			set_maximized(shsurf, true);
+			dirty = true;
+		}
+
+		if (dirty) {
+			weston_view_geometry_dirty(view);
+			weston_surface_damage(view->surface);
+		}
+	}
+}
 
 static void
 handle_output_resized(struct wl_listener *listener, void *data)
@@ -4394,8 +4460,13 @@ handle_output_resized(struct wl_listener *listener, void *data)
 	if (!sh_output)
 		return;
 
+	if (shell->lock_surface)
+		shell->lock_surface->committed(shell->lock_surface, 0, 0);
+
 	shell_resize_surface_to_output(shell, sh_output->background_surface, output);
 	shell_resize_surface_to_output(shell, sh_output->panel_surface, output);
+
+	shell_for_each_layer(shell, handle_output_resize_layer, data);
 }
 
 static void
@@ -4444,7 +4515,9 @@ handle_output_move_layer(struct desktop_shell *shell,
 
 		x = view->geometry.x + output->move_x;
 		y = view->geometry.y + output->move_y;
-		weston_view_set_position(view, x, y);
+
+		if (pixman_region32_contains_point(&output->region, x, y, NULL))
+			weston_view_set_position(view, x, y);
 	}
 }
 
