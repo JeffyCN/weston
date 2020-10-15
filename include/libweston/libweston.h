adeeab4d325d213fca476c8f30a3715d2b2bb5fa
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-10-15 12:43:00 +0800

    compositor: Support setting default layer for the first app
    Set env "WESTON_FIRST_APP_LAYER" to "top" or "bottom".
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- include/libweston/libweston.h
+++ include/libweston/libweston.h
@@ -1880,6 +1880,9 @@ enum weston_surface_flags {
 	SURFACE_ACTIVATE	= 1 << 5,
 };
 
+#define WESTON_SURFACE_FLAGS_STAY_MASK \
+	((uint32_t)(SURFACE_STAY_ON_TOP | SURFACE_STAY_ON_BOTTOM))
+
 struct weston_surface {
 	struct wl_resource *resource;
 	struct wl_signal destroy_signal; /* callback argument: this surface */
