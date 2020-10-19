JeffyCN/weston

commit 3623e46dc555ea9df34ff259530af64f0d9f0645

desktop-shell: Set 1x1 buffers for solid-color backgrounds
When we are going to set a solid color for the background, use
a 1x1 buffer and set the viewport to the full size. This avoids
un-necessary allocation of buffer memory.

Signed-off-by: Harish Krupo <harish.krupo.kps@intel.com>

clients/desktop-shell.c
@@ -849,6 +849,12 @@ background_configure(void *data,
 		return;
 	}
 
+	if (!background->image) {
+		widget_set_viewport_destination(background->widget, width, height);
+		width = 1;
+		height = 1;
+	}
+
 	widget_schedule_resize(background->widget, width, height);
 }
 

commit 8ef3ce5c6753bcc91dacac2a6d6a8357dc73ea09

clients: Fix shell background image setting.
If weston.ini is not setting background-image path,
then desktop-shell sets ${DATDIR}/weston/pattern.png
as background. However in this case  width and height
is set to 1 during background config and is being
scaled to avoid allocation of buffer.

This behavior is not right. Along with background-image
path, we should also check if background-color is set.
If background color is set, then only scale 1x1 buffer.

This would allow to set pattern.png as default wallpaper
of weston correctly, if background-color is also not set
in weston.ini file.

Fixes: 3623e46dc555ea9df34ff259530af64f0d9f0645
Signed-off-by: Tanmay Shah <tanmay@codeaurora.org>

clients/desktop-shell.c
@@ -849,7 +849,7 @@ background_configure(void *data,
 		return;
 	}
 
-	if (!background->image) {
+	if (!background->image && background->color) {
 		widget_set_viewport_destination(background->widget, width, height);
 		width = 1;
 		height = 1;