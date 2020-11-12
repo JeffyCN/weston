df336262b0f2f5812b28ec7c4ccb1b0cf83430f4
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-11-12 16:59:50 +0800

    backend-drm: Add dummy output when no screens connected
    Some clients are not expecting no screens, add a dummy output for them.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- desktop-shell/shell.c
+++ desktop-shell/shell.c
@@ -4555,11 +4555,6 @@ shell_reposition_view_on_output_change(struct weston_view *view)
 		    shsurf->state.fullscreen)
 			return;
 	}
-
-
-	shsurf->saved_position_valid = false;
-	set_maximized(shsurf, false);
-	set_fullscreen(shsurf, false, NULL);
 }
 
 void
@@ -4659,6 +4654,9 @@ handle_output_resized(struct wl_listener *listener, void *data)
 		container_of(listener, struct desktop_shell, resized_listener);
 	struct weston_output *output = (struct weston_output *)data;
 	struct shell_output *sh_output = find_shell_output_from_weston_output(shell, output);
+
+	if (!sh_output)
+		return;
 
 	if (shell->lock_surface) {
 		struct weston_coord_surface offset =
