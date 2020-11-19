3d7817576a9f057f64adf945823314341334b91d
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-11-19 09:41:47 +0800

    backend-drm: Support mirror mode
    Set env "WESTON_DRM_MIRROR" to enable mirror mode, and set env
    "WESTON_DRM_KEEP_RATIO" to keep the aspect ratio.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- desktop-shell/shell.c
+++ desktop-shell/shell.c
@@ -4280,6 +4280,9 @@ weston_view_set_initial_position(struct weston_view *view,
 	}
 
 	wl_list_for_each(output, &compositor->output_list, link) {
+		if (output->unavailable)
+			continue;
+
 		if (pixman_region32_contains_point(&output->region, ix, iy, NULL)) {
 			target_output = output;
 			break;
@@ -4799,7 +4802,7 @@ shell_reposition_view_on_output_change(struct weston_view *view)
 	if (!shsurf)
 		return;
 
-	if (shsurf->fullscreen_output && shsurf->fullscreen_output->destroying)
+	if (!weston_output_valid(shsurf->fullscreen_output))
 		shsurf->fullscreen_output = NULL;
 }
 
