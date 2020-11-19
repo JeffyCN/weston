a8f72f4f0349a02a19425553b677f96332c54a87
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-11-19 09:41:47 +0800

    backend-drm: Support mirror mode
    Set env "WESTON_DRM_MIRROR" to enable mirror mode, and set env
    "WESTON_DRM_KEEP_RATIO" to keep the aspect ratio.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libweston/input.c
+++ libweston/input.c
@@ -2153,20 +2153,28 @@ WL_EXPORT struct weston_coord_global
 weston_pointer_clamp(struct weston_pointer *pointer, struct weston_coord_global pos)
 {
 	struct weston_compositor *ec = pointer->seat->compositor;
-	struct weston_output *output, *prev = NULL;
+	struct weston_output *output, *prev = NULL, *fallback = NULL;
 	int valid = 0;
 
 	wl_list_for_each(output, &ec->output_list, link) {
 		if (pointer->seat->output && pointer->seat->output != output)
 			continue;
+
+		if (!weston_output_valid(output))
+			continue;
+
 		if (weston_output_contains_coord(output, pos))
 			valid = 1;
 		if (weston_output_contains_coord(output, pointer->pos))
 			prev = output;
+		if (!fallback)
+			fallback = output;
 	}
 
 	if (!prev)
 		prev = pointer->seat->output;
+	if (!prev)
+		prev = fallback;
 
 	if (prev && !valid)
 		pos = weston_pointer_clamp_for_output(pointer, prev, pos);
@@ -2224,6 +2232,9 @@ weston_pointer_handle_output_destroy(struct wl_listener *listener, void *data)
 
 	wl_list_for_each(output, &ec->output_list, link) {
 		int ox, oy;
+
+		if (!weston_output_valid(output))
+			continue;
 
 		if (weston_output_contains_coord(output, pointer->pos))
 			return;
