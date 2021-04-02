300842f26c46d89da5b641b219ee06844567caf9
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-04-02 09:49:09 +0800

    libinput-seat: Improve input device and output associating
    An input device can associate with an output using udev rules's
    WL_OUTPUT property, for example:
    ATTRS{idVendor}=="0eef", ATTRS{idProduct}=="0001", ENV{WL_OUTPUT}="HDMI-A-1"
    
    An input device can be added into a wl_seat using udev rules's WL_SEAT
    property, for example:
    ATTRS{idVendor}=="0eef", ATTRS{idProduct}=="0001", ENV{WL_SEAT}="seat1"
    
    An output can associate with a wl_seat using seat entry in weston.ini's
    output section, for example:
    [output]
    name=HDMI-A-1
    seat=seat1
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libweston/backend-drm/drm.c
+++ libweston/backend-drm/drm.c
@@ -1679,6 +1679,7 @@ setup_output_seat_constraint(struct drm_backend *b,
 			return;
 
 		seat->base.output = output;
+		seat->has_output = true;
 
 		pointer = weston_seat_get_pointer(&seat->base);
 		if (pointer)
