54a7cbce5ba22eca3ef6b568ed1104394cdac632
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-04-07 08:25:57 +0800

    config-parser: Add support for loading multiple config files
    Add support for loading additional .ini configs from "<config>.d/"
    directory, with proper merging and override semantics.
    
    Tested with:
    /etc/xdg/weston/weston.ini.d/99-pixman.ini
    [core]
    use-pixman=true
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- frontend/main.c
+++ frontend/main.c
@@ -1299,8 +1299,12 @@ load_configuration(struct weston_config **config, int32_t noconfig,
 	if (config_file)
 		file = config_file;
 
-	if (noconfig == 0)
+	if (noconfig == 0) {
+		/* Enable verbose parsing output for main config only */
+		setenv("WESTON_MAIN_PARSE", "1", 1);
 		*config = weston_config_parse(file);
+		unsetenv("WESTON_MAIN_PARSE");
+	}
 
 	if (*config) {
 		full_path = weston_config_get_full_path(*config);
