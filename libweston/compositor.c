JeffyCN/weston

commit 0e4f097d0837c5db96f15e137ab131b19f794411

compositor: clip the opaque region with the scissor area
The opaque region is used to determine where the views underneath the current
view must be drawn. If the opaque is not clipped, then the area that is part of
the opaque region but not part of the scissor area is not drawn at all.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

libweston/compositor.c
@@ -1467,6 +1467,9 @@ weston_view_update_transform_disable(struct weston_view *view)
 	if (view->alpha == 1.0) {
 		pixman_region32_copy(&view->transform.opaque,
 				     &view->surface->opaque);
+		pixman_region32_intersect(&view->transform.opaque,
+					  &view->transform.opaque,
+					  &view->geometry.scissor);
 		pixman_region32_translate(&view->transform.opaque,
 					  view->geometry.x,
 					  view->geometry.y);
@@ -1504,15 +1507,6 @@ weston_view_update_transform_enable(struct weston_view *view)
 		return -1;
 	}
 
-	if (view->alpha == 1.0 &&
-	    matrix->type == WESTON_MATRIX_TRANSFORM_TRANSLATE) {
-		pixman_region32_copy(&view->transform.opaque,
-				     &view->surface->opaque);
-		pixman_region32_translate(&view->transform.opaque,
-					  matrix->d[12],
-					  matrix->d[13]);
-	}
-
 	pixman_region32_init_rect(&surfregion, 0, 0,
 				  view->surface->width, view->surface->height);
 
@@ -1522,6 +1516,18 @@ weston_view_update_transform_enable(struct weston_view *view)
 
 	view_compute_bbox(view, surfbox, &view->transform.boundingbox);
 	pixman_region32_fini(&surfregion);
+
+	if (view->alpha == 1.0 &&
+	    matrix->type == WESTON_MATRIX_TRANSFORM_TRANSLATE) {
+		pixman_region32_copy(&view->transform.opaque,
+				     &view->surface->opaque);
+		pixman_region32_intersect(&view->transform.opaque,
+					  &view->transform.opaque,
+					  &view->geometry.scissor);
+		pixman_region32_translate(&view->transform.opaque,
+					  matrix->d[12],
+					  matrix->d[13]);
+	}
 
 	return 0;
 }

commit 713f02327d6c3d1c34f768282034989c1b6769d9

compositor: Fix opaque regions
Commit 0e4f097d broke opaque regions, and since then weston will waste
time rendering occluded areas.

I think this is because we're taking the intersection of the opaque
and scissor regions even when the scissor region isn't enabled.

An easy test is to turn on triangle fan debugging with the gl renderer,
then run weston-simple-damage and move another opaque application such as
weston-terminal over it.

Signed-off-by: Derek Foreman <derek.foreman@collabora.com>

libweston/compositor.c
@@ -1522,9 +1522,10 @@ weston_view_update_transform_disable(struct weston_view *view)
 	if (view->alpha == 1.0) {
 		pixman_region32_copy(&view->transform.opaque,
 				     &view->surface->opaque);
-		pixman_region32_intersect(&view->transform.opaque,
-					  &view->transform.opaque,
-					  &view->geometry.scissor);
+		if (view->geometry.scissor_enabled)
+			pixman_region32_intersect(&view->transform.opaque,
+						  &view->transform.opaque,
+						  &view->geometry.scissor);
 		pixman_region32_translate(&view->transform.opaque,
 					  view->geometry.x,
 					  view->geometry.y);
@@ -1576,9 +1577,10 @@ weston_view_update_transform_enable(struct weston_view *view)
 	    matrix->type == WESTON_MATRIX_TRANSFORM_TRANSLATE) {
 		pixman_region32_copy(&view->transform.opaque,
 				     &view->surface->opaque);
-		pixman_region32_intersect(&view->transform.opaque,
-					  &view->transform.opaque,
-					  &view->geometry.scissor);
+		if (view->geometry.scissor_enabled)
+			pixman_region32_intersect(&view->transform.opaque,
+						  &view->transform.opaque,
+						  &view->geometry.scissor);
 		pixman_region32_translate(&view->transform.opaque,
 					  matrix->d[12],
 					  matrix->d[13]);