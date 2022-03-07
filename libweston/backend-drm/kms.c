a20a08af25e89f8e9333502ef66034e8c1c765a8
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-03-07 17:20:59 +0800

    backend-drm: Allow overriding plane type when using atomic
    Set env DRM_PLANE_TYPE_<plane id> to primary|overlay|cursor.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libweston/backend-drm/kms.c
+++ libweston/backend-drm/kms.c
@@ -461,6 +461,35 @@ drm_property_info_free(struct drm_property_info *info, int num_props)
 	memset(info, 0, sizeof(*info) * num_props);
 }
 
+enum wdrm_plane_type
+drm_plane_get_type(struct drm_plane *plane, drmModeObjectProperties *props)
+{
+	struct drm_device *device = plane->device;
+	enum wdrm_plane_type type;
+	const char *env;
+	char buf[256];
+
+	type = drm_property_get_value(&plane->props[WDRM_PLANE_TYPE],
+				      props,
+				      WDRM_PLANE_TYPE__COUNT);
+	if (!device->atomic_modeset)
+		return type;
+
+	snprintf(buf, sizeof(buf), "DRM_PLANE_TYPE_%d", plane->plane_id);
+	env = getenv(buf);
+	if (!env)
+		return type;
+
+	if (!strcmp(env, "primary"))
+		return WDRM_PLANE_TYPE_PRIMARY;
+	else if (!strcmp(env, "overlay"))
+		return WDRM_PLANE_TYPE_OVERLAY;
+	else if (!strcmp(env, "cursor"))
+		return WDRM_PLANE_TYPE_CURSOR;
+
+	return type;
+}
+
 /**
  * Populates the plane's formats array, using either the IN_FORMATS blob
  * property (if available), or the plane's format list if not.
