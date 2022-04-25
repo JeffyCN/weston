JeffyCN/weston

commit 0b51b02c5e67bc26baead8e6b8bbadef16e81f66

gl-renderer: Don't re-import dmabufs
My reading of the GL spec is that a dmabuf becomes a sibling to the
EGLImage created from it, and that all updates to the dmabuf will be
propagated to the EGLImage.

A rebind is still required every time the dmabuf content changes,
but this should be satisfied by gl_renderer_attach(), which does
a rebind when the buffer is commit.

Signed-off-by: Derek Foreman <derek.foreman@collabora.com>

libweston/renderer-gl/gl-renderer.c
@@ -2733,35 +2733,6 @@ gl_renderer_import_dmabuf(struct weston_compositor *ec,
 }
 
 static bool
-import_known_dmabuf(struct gl_renderer *gr,
-                    struct dmabuf_image *image)
-{
-	switch (image->import_type) {
-	case IMPORT_TYPE_DIRECT:
-		image->images[0] = import_simple_dmabuf(gr, &image->dmabuf->attributes);
-		if (!image->images[0]) {
-			/* num_images is already set to 1 when doing the intial
-			 * import so reset it to 0 to avoid passing an empty
-			 * egl_image to dmabuf_destroy */
-			image->num_images = 0;
-			return false;
-		}
-		break;
-
-	case IMPORT_TYPE_GL_CONVERSION:
-		if (!import_yuv_dmabuf(gr, image))
-			return false;
-		break;
-
-	default:
-		weston_log("Invalid import type for dmabuf\n");
-		return false;
-	}
-
-	return true;
-}
-
-static bool
 dmabuf_is_opaque(struct linux_dmabuf_buffer *dmabuf)
 {
 	const struct pixel_format_info *info;
@@ -2828,14 +2799,6 @@ gl_renderer_attach_dmabuf(struct weston_surface *surface,
 
 	/* The dmabuf_image should have been created during the import */
 	assert(image != NULL);
-
-	for (i = 0; i < image->num_images; ++i)
-		egl_image_unref(image->images[i]);
-
-	if (!import_known_dmabuf(gr, image)) {
-		linux_dmabuf_buffer_send_server_error(dmabuf, "EGL dmabuf import failed");
-		return;
-	}
 
 	gs->num_images = image->num_images;
 	for (i = 0; i < gs->num_images; ++i)

commit f8ac6f940fa229adbb17023a6840eeca1da64bbd

gl-renderer: Remove outdated comment
The comment about needing to have destroyed images is somewhat less true
now that we actively avoid doing so.

Signed-off-by: Daniel Stone <daniels@collabora.com>
Fixes: 0b51b02c5e67 ("gl-renderer: Don't re-import dmabufs")

libweston/renderer-gl/gl-renderer.c
@@ -2843,14 +2843,6 @@ gl_renderer_attach_dmabuf(struct weston_surface *surface,
 	gs->direct_display = dmabuf->direct_display;
 	surface->is_opaque = dmabuf_is_opaque(dmabuf);
 
-	/*
-	 * We try to always hold an imported EGLImage from the dmabuf
-	 * to prevent the client from preventing re-imports. But, we also
-	 * need to re-import every time the contents may change because
-	 * GL driver's caching may need flushing.
-	 *
-	 * Here we release the cache reference which has to be final.
-	 */
 	if (dmabuf->direct_display)
 		return;
 