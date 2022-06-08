d74037d409be66a88c5277ab0039b04c17975ad2
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-06-08 09:53:19 +0800

    desktop-shell: Fix crash when destroying surface with null fade
    Add null check for wsurface_anim_fade to prevent crash when destroying
    surfaces that haven't committed animation states.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- desktop-shell/shell.c
+++ desktop-shell/shell.c
@@ -158,6 +158,10 @@ desktop_shell_destroy_surface(struct shell_surface *shsurf)
 	wl_signal_emit_mutable(&shsurf->destroy_signal, shsurf);
 	weston_surface_unref(shsurf->wsurface_anim_fade);
 
+	/* wsurface_anim_fade may be NULL if surface was never committed */
+	if (shsurf->wsurface_anim_fade)
+		weston_surface_unref(shsurf->wsurface_anim_fade);
+
 	if (shsurf->output_destroy_listener.notify) {
 		wl_list_remove(&shsurf->output_destroy_listener.link);
 		shsurf->output_destroy_listener.notify = NULL;
