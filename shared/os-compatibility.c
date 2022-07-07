99b2b958f9f7aab23f5ce2e221ceba824116e5b2
Pekka Paalanen <pekka.paalanen@collabora.com>
2022-07-07 11:34:44 +0300

    shared: introduce os_fd_clear_cloexec()
    This function will be used between fork() and exec() to remove the
    close-on-exec flag. The first user will be compositor/xwayland.c.
    
    Signed-off-by: Pekka Paalanen <pekka.paalanen@collabora.com>

--- shared/os-compatibility.c
+++ shared/os-compatibility.c
@@ -41,6 +41,21 @@
 #define READONLY_SEALS (F_SEAL_SHRINK | F_SEAL_GROW | F_SEAL_WRITE)
 
 int
+os_fd_clear_cloexec(int fd)
+{
+	int flags;
+
+	flags = fcntl(fd, F_GETFD);
+	if (flags == -1)
+		return -1;
+
+	if (fcntl(fd, F_SETFD, flags & ~(int)FD_CLOEXEC) == -1)
+		return -1;
+
+	return 0;
+}
+
+int
 os_fd_set_cloexec(int fd)
 {
 	int flags;
