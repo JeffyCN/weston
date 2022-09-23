b5d8077a3b48fbfaa2014f9b9394b2549dacb0d3
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-23 17:24:12 +0800

    HACK: Honour cursor-size config
    By scaling the cursor surface.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- include/libweston/libweston.h
+++ include/libweston/libweston.h
@@ -809,6 +809,8 @@ struct weston_pointer {
 	struct wl_listener output_destroy_listener;
 
 	struct wl_list timestamps_list;
+
+	float scale;
 };
 
 /** libinput style calibration matrix
@@ -1547,6 +1549,8 @@ struct weston_compositor {
 	bool hide_cursor;
 
 	uint32_t pending_fade_out;
+
+	int cursor_size;
 };
 
 struct weston_solid_buffer_values {
@@ -1605,7 +1609,7 @@ struct weston_buffer_viewport {
 		uint32_t transform;
 
 		/* wl_surface.set_scaling_factor */
-		int32_t scale;
+		float scale;
 
 		/*
 		 * If src_width != wl_fixed_from_int(-1),
