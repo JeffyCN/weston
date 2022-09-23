71667c28e7d792196bf2bea593439c797e516553
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-23 17:24:12 +0800

    HACK: Honour cursor-size config
    By scaling the cursor surface.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libweston/libweston-internal.h
+++ libweston/libweston-internal.h
@@ -583,7 +583,7 @@ void
 convert_size_by_transform_scale(int32_t *width_out, int32_t *height_out,
 				int32_t width, int32_t height,
 				uint32_t transform,
-				int32_t scale);
+				float scale);
 
 /* User authentication for remote backends */
 
