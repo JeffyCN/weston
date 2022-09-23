875dcbcfb01a6065f83e9dd4ab7a09dc2885054b
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-23 17:24:12 +0800

    HACK: Honour cursor-size config
    By scaling the cursor surface.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libweston/backend-drm/state-propose.c
+++ libweston/backend-drm/state-propose.c
@@ -201,10 +201,8 @@ drm_output_prepare_cursor_paint_node(struct drm_output_state *output_state,
 	    plane_state->src_h > (unsigned) device->cursor_height << 16 ||
 	    plane_state->src_w != plane_state->dest_w << 16 ||
 	    plane_state->src_h != plane_state->dest_h << 16) {
-		drm_debug(b, "\t\t\t\t[%s] not assigning view %p to %s plane "
-			     "(positioning requires cropping or scaling)\n",
-			     p_name, ev, p_name);
-		goto err;
+		if (!device->atomic_modeset)
+			goto err_scale;
 	}
 
 	drm_output_set_cursor_view(output, ev);
@@ -218,6 +216,28 @@ drm_output_prepare_cursor_paint_node(struct drm_output_state *output_state,
 	 */
 	plane_state->fb = drm_fb_ref(output->gbm_cursor_fb[0]);
 
+	if (device->atomic_modeset) {
+		float scale =
+			1.0 * b->compositor->cursor_size / ev->surface->width;
+
+		/* HACK: The HW might not be able to crop it */
+		if ((plane_state->src_w >> 16) < 4 ||
+		    (plane_state->src_h >> 16) < 4)
+			goto err_scale;
+
+		plane_state->dest_w *= scale;
+		plane_state->dest_h *= scale;
+
+		/* HACK: The HW might not be able to scale it */
+		if (plane_state->dest_w > 8 * (plane_state->src_w >> 16) ||
+		    plane_state->dest_w * 8 < (plane_state->src_w >> 16) ||
+		    plane_state->dest_h > 8 * (plane_state->src_h >> 16) ||
+		    plane_state->dest_h * 8 < (plane_state->src_h >> 16))
+			goto err_scale;
+
+		goto out;
+	}
+
 	/* The cursor API is somewhat special: in cursor_bo_update(), we upload
 	 * a buffer which is always cursor_width x cursor_height, even if the
 	 * surface we want to promote is actually smaller than this. Manually
@@ -227,11 +247,16 @@ drm_output_prepare_cursor_paint_node(struct drm_output_state *output_state,
 	plane_state->dest_w = device->cursor_width;
 	plane_state->dest_h = device->cursor_height;
 
+out:
 	drm_debug(b, "\t\t\t\t[%s] provisionally assigned view %p to cursor\n",
 		  p_name, ev);
 
 	return plane_state;
 
+err_scale:
+	drm_debug(b, "\t\t\t\t[%s] not assigning view %p to %s plane "
+		  "(positioning requires cropping or scaling)\n",
+		  p_name, ev, p_name);
 err:
 	drm_plane_state_put_back(plane_state);
 	return NULL;
