48d187b7a8c923aed79ec03d76a35ba08603b597
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-10-03 21:03:04 +0800

    desktop-shell: Fix crash when hotplugging screens
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- desktop-shell/shell.c
+++ desktop-shell/shell.c
@@ -3937,6 +3937,8 @@ check_desktop_shell_crash_too_early(struct desktop_shell *shell)
 	if (clock_gettime(CLOCK_MONOTONIC, &now) < 0)
 		return false;
 
+	/* HACK: The shell might be crashed too early when hotplugging */
+#if 0
 	/*
 	 * If the shell helper client dies before the session has been
 	 * up for roughly 30 seconds, better just make Weston shut down,
@@ -3952,6 +3954,7 @@ check_desktop_shell_crash_too_early(struct desktop_shell *shell)
 
 		return true;
 	}
+#endif
 
 	return false;
 }
@@ -4391,10 +4394,14 @@ shell_output_destroy(struct shell_output *shell_output)
 	if (shell_output->fade.startup_timer)
 		wl_event_source_remove(shell_output->fade.startup_timer);
 
-	if (shell_output->panel_surface)
+	if (shell_output->panel_surface) {
 		wl_list_remove(&shell_output->panel_surface_listener.link);
-	if (shell_output->background_surface)
+		shell_output->panel_surface->committed = NULL;
+	}
+	if (shell_output->background_surface) {
 		wl_list_remove(&shell_output->background_surface_listener.link);
+		shell_output->background_surface->committed = NULL;
+	}
 	wl_list_remove(&shell_output->destroy_listener.link);
 	wl_list_remove(&shell_output->link);
 	free(shell_output);
@@ -4576,7 +4583,7 @@ setup_output_destroy_handler(struct weston_compositor *ec,
 static void
 desktop_shell_destroy_layer(struct weston_layer *layer)
 {
-	struct weston_view *view;
+	struct weston_view *view, *tmp;
 	bool removed;
 
 	do {
@@ -4601,9 +4608,11 @@ desktop_shell_destroy_layer(struct weston_layer *layer)
 		 * we restart the loop as long as we keep removing views from
 		 * the list.
 		 */
-		wl_list_for_each(view, &layer->view_list.link, layer_link.link) {
+		wl_list_for_each_safe(view, tmp, &layer->view_list.link,
+				      layer_link.link) {
 			struct shell_surface *shsurf =
 				get_shell_surface(view->surface);
+			weston_layer_entry_remove(&view->layer_link);
 			if (shsurf) {
 				desktop_shell_destroy_surface(shsurf);
 				removed = true;
