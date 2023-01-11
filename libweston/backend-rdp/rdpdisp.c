JeffyCN/weston

commit 70b03b292805b7ba03a08b623b94e27cdbd0aad9

rdp: Add preliminary rdp multihead support
This sets up monitor layout callbacks, and enables input event translation
between the RDP space and the weston desktop. The RDP backend now uses
a heads changed callback instead of the simple head configurator.

We only allow a single monitor for now, but in the future RAIL will make
use of multi-head.

As a side effect, scaling is now supported in RDP sessions.

It should be noted that due to differences between RDP and wayland
representation of their global coordinate spaces, mixing DPI leads to
RDP monitor layouts that can't properly be represented in weston.

Co-authored-by: Steve Pronovost <spronovo@microsoft.com>
Co-authored-by: Brenton DeGeer <brdegeer@microsoft.com>
Signed-off-by: Hideyuki Nagase <hideyukn@microsoft.com>
Signed-off-by: Steve Pronovost <spronovo@microsoft.com>
Signed-off-by: Brenton DeGeer <brdegeer@microsoft.com>

libweston/backend-rdp/rdpdisp.c
@@ -1,0 +1,356 @@
+/*
+ * Copyright © 2020 Microsoft
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "config.h"
+
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include <stdio.h>
+#include <wchar.h>
+#include <strings.h>
+
+#include "rdp.h"
+
+#include "shared/xalloc.h"
+
+static bool
+match_primary(struct rdp_backend *rdp, rdpMonitor *a, rdpMonitor *b)
+{
+	if (a->is_primary && b->is_primary)
+		return true;
+
+	return false;
+}
+
+static bool
+match_dimensions(struct rdp_backend *rdp, rdpMonitor *a, rdpMonitor *b)
+{
+	int scale_a = a->attributes.desktopScaleFactor;
+	int scale_b = b->attributes.desktopScaleFactor;
+
+
+	if (a->width != b->width ||
+	    a->height != b->height ||
+	    scale_a != scale_b)
+		return false;
+
+	return true;
+}
+
+static bool
+match_position(struct rdp_backend *rdp, rdpMonitor *a, rdpMonitor *b)
+{
+	if (a->x != b->x ||
+	    a->y != b->y)
+		return false;
+
+	return true;
+}
+
+static bool
+match_exact(struct rdp_backend *rdp, rdpMonitor *a, rdpMonitor *b)
+{
+	if (match_dimensions(rdp, a, b) &&
+	    match_position(rdp, a, b))
+		return true;
+
+	return false;
+}
+
+static bool
+match_any(struct rdp_backend *rdp, rdpMonitor *a, rdpMonitor *b)
+{
+	return true;
+}
+
+static void
+update_head(struct rdp_backend *rdp, struct rdp_head *head, rdpMonitor *config)
+{
+	struct weston_mode mode = {};
+	int scale;
+	bool changed = false;
+
+	head->matched = true;
+	scale = config->attributes.desktopScaleFactor / 100;
+	scale = scale ? scale : 1;
+
+	if (!match_position(rdp, &head->config, config))
+		changed = true;
+
+	if (!match_dimensions(rdp, &head->config, config)) {
+		mode.flags = WL_OUTPUT_MODE_PREFERRED;
+		mode.width = config->width;
+		mode.height = config->height;
+		mode.refresh = rdp->rdp_monitor_refresh_rate;
+		weston_output_mode_set_native(head->base.output,
+					      &mode, scale);
+		changed = true;
+	}
+
+	if (changed) {
+		weston_head_set_device_changed(&head->base);
+	}
+	head->config = *config;
+}
+
+static void
+match_heads(struct rdp_backend *rdp, rdpMonitor *config, uint32_t count,
+	    int *done,
+	    bool (*cmp)(struct rdp_backend *rdp, rdpMonitor *a, rdpMonitor *b))
+{
+	struct weston_head *iter;
+	struct rdp_head *current;
+	uint32_t i;
+
+	wl_list_for_each(iter, &rdp->compositor->head_list, compositor_link) {
+		current = to_rdp_head(iter);
+		if (current->matched)
+			continue;
+
+		for (i = 0; i < count; i++) {
+			if (*done & (1 << i))
+				continue;
+
+			if (cmp(rdp, &current->config, &config[i])) {
+				*done |= 1 << i;
+				update_head(rdp, current, &config[i]);
+				break;
+			}
+		}
+	}
+}
+
+static void
+disp_layout_change(freerdp_peer *client, rdpMonitor *config, UINT32 monitorCount)
+{
+	RdpPeerContext *peerCtx = (RdpPeerContext *)client->context;
+	struct rdp_backend *b = peerCtx->rdpBackend;
+	struct rdp_head *current;
+	struct weston_head *iter, *tmp;
+	pixman_region32_t desktop;
+	int done = 0;
+
+	assert_compositor_thread(b);
+
+	pixman_region32_init(&desktop);
+
+	/* Prune heads that were never enabled, and flag heads as unmatched  */
+	wl_list_for_each_safe(iter, tmp, &b->compositor->head_list, compositor_link) {
+		current = to_rdp_head(iter);
+		if (!iter->output) {
+			rdp_head_destroy(iter);
+			continue;
+		}
+		current->matched = false;
+	}
+
+	/* We want the primary head to remain primary - it
+	 * should always be rdp-0.
+	  */
+	match_heads(b, config, monitorCount, &done, match_primary);
+
+	/* Look for any exact match */
+	match_heads(b, config, monitorCount, &done, match_exact);
+
+	/* Match first head with the same dimensions */
+	match_heads(b, config, monitorCount, &done, match_dimensions);
+
+	/* Match head with the same position */
+	match_heads(b, config, monitorCount, &done, match_position);
+
+	/* Pick any available head */
+	match_heads(b, config, monitorCount, &done, match_any);
+
+	/* Destroy any heads we won't be using */
+	wl_list_for_each_safe(iter, tmp, &b->compositor->head_list, compositor_link) {
+		current = to_rdp_head(iter);
+		if (!current->matched)
+			rdp_head_destroy(iter);
+	}
+
+
+	for (uint32_t i = 0; i < monitorCount; i++) {
+		/* accumulate monitor layout */
+		pixman_region32_union_rect(&desktop, &desktop,
+					   config[i].x,
+					   config[i].y,
+					   config[i].width,
+					   config[i].height);
+
+		/* Create new heads for any without matches */
+		if (!(done & (1 << i)))
+			rdp_head_create(b->compositor, &config[i]);
+	}
+	peerCtx->desktop_left = desktop.extents.x1;
+	peerCtx->desktop_top = desktop.extents.y1;
+	peerCtx->desktop_width = desktop.extents.x2 - desktop.extents.x1;
+	peerCtx->desktop_height = desktop.extents.y2 - desktop.extents.y1;
+	pixman_region32_fini(&desktop);
+}
+
+static bool
+disp_sanity_check_layout(RdpPeerContext *peerCtx, rdpMonitor *config, uint32_t count)
+{
+	struct rdp_backend *b = peerCtx->rdpBackend;
+	uint32_t primaryCount = 0;
+	uint32_t i;
+
+	/* dump client monitor topology */
+	rdp_debug(b, "%s:---INPUT---\n", __func__);
+	for (i = 0; i < count; i++) {
+		int scale = config[i].attributes.desktopScaleFactor / 100;
+
+		rdp_debug(b, "	rdpMonitor[%d]: x:%d, y:%d, width:%d, height:%d, is_primary:%d\n",
+			i, config[i].x, config[i].y,
+			   config[i].width, config[i].height,
+			   config[i].is_primary);
+		rdp_debug(b, "	rdpMonitor[%d]: physicalWidth:%d, physicalHeight:%d, orientation:%d\n",
+			i, config[i].attributes.physicalWidth,
+			   config[i].attributes.physicalHeight,
+			   config[i].attributes.orientation);
+		rdp_debug(b, "	rdpMonitor[%d]: desktopScaleFactor:%d, deviceScaleFactor:%d\n",
+			i, config[i].attributes.desktopScaleFactor,
+			   config[i].attributes.deviceScaleFactor);
+
+		rdp_debug(b, "	rdpMonitor[%d]: scale:%d\n",
+			i, scale);
+	}
+
+	for (i = 0; i < count; i++) {
+		/* make sure there is only one primary and its position at client */
+		if (config[i].is_primary) {
+			/* count number of primary */
+			if (++primaryCount > 1) {
+				weston_log("%s: RDP client reported unexpected primary count (%d)\n",
+					   __func__, primaryCount);
+				return false;
+			}
+			/* primary must be at (0,0) in client space */
+			if (config[i].x != 0 || config[i].y != 0) {
+				weston_log("%s: RDP client reported primary is not at (0,0) but (%d,%d).\n",
+					   __func__, config[i].x, config[i].y);
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool
+handle_adjust_monitor_layout(freerdp_peer *client, int monitor_count, rdpMonitor *monitors)
+{
+	RdpPeerContext *peerCtx = (RdpPeerContext *)client->context;
+
+	if (!disp_sanity_check_layout(peerCtx, monitors, monitor_count))
+		return true;
+
+	disp_layout_change(client, monitors, monitor_count);
+
+	return true;
+}
+
+static bool
+rect_contains(int32_t px, int32_t py,  int32_t rx, int32_t ry,
+	      int32_t width, int32_t height)
+{
+	if (px < rx)
+		return false;
+	if (py < ry)
+		return false;
+	if (px >= rx + width)
+		return false;
+	if (py >= ry + height)
+		return false;
+
+	return true;
+}
+
+static bool
+rdp_head_contains(struct rdp_head *rdp_head, int32_t x, int32_t y)
+{
+	rdpMonitor *config = &rdp_head->config;
+
+	/* If we're forcing RDP desktop size then we don't have
+	 * useful information in the monitor structs, but we
+	 * can rely on the output settings in that case.
+	 */
+	if (config->width == 0) {
+		struct weston_head *head = &rdp_head->base;
+		struct weston_output *output = head->output;
+
+		if (!output)
+			return false;
+
+		return rect_contains(x, y, output->x, output->y,
+				     output->width * output->scale,
+				     output->height * output->scale);
+	}
+
+	return rect_contains(x, y, config->x, config->y,
+			     config->width, config->height);
+}
+
+/* Input x/y in client space, output x/y in weston space */
+struct weston_output *
+to_weston_coordinate(RdpPeerContext *peerContext, int32_t *x, int32_t *y)
+{
+	struct rdp_backend *b = peerContext->rdpBackend;
+	int sx = *x, sy = *y;
+	struct weston_head *head_iter;
+
+	/* First, find which monitor contains this x/y. */
+	wl_list_for_each(head_iter, &b->compositor->head_list, compositor_link) {
+		struct rdp_head *head = to_rdp_head(head_iter);
+
+		assert(head);
+
+		if (rdp_head_contains(head, sx, sy)) {
+			struct weston_output *output = head->base.output;
+			float scale = 1.0f / head->base.output->scale;
+
+			sx -= head->config.x;
+			sy -= head->config.y;
+			/* scale x/y to client output space. */
+			sx *= scale;
+			sy *= scale;
+			/* translate x/y to offset of this output in weston space. */
+			sx += output->x;
+			sy += output->y;
+			rdp_debug_verbose(b, "%s: (x:%d, y:%d) -> (sx:%d, sy:%d) at head:%s\n",
+					  __func__, *x, *y, sx, sy, head->base.name);
+			*x = sx;
+			*y = sy;
+			return output;
+		}
+	}
+	/* x/y is outside of any monitors. */
+	return NULL;
+}

compositor/main.c
@@ -3070,40 +3070,6 @@ load_headless_backend(struct weston_compositor *c,
 	return 0;
 }
 
-static int
-rdp_backend_output_configure(struct weston_output *output)
-{
-	struct wet_compositor *compositor = to_wet_compositor(output->compositor);
-	struct wet_output_config *parsed_options = compositor->parsed_options;
-	const struct weston_rdp_output_api *api = weston_rdp_output_get_api(output->compositor);
-	int width = 640;
-	int height = 480;
-
-	assert(parsed_options);
-
-	if (!api) {
-		weston_log("Cannot use weston_rdp_output_api.\n");
-		return -1;
-	}
-
-	if (parsed_options->width)
-		width = parsed_options->width;
-
-	if (parsed_options->height)
-		height = parsed_options->height;
-
-	weston_output_set_scale(output, 1);
-	weston_output_set_transform(output, WL_OUTPUT_TRANSFORM_NORMAL);
-
-	if (api->output_set_size(output, width, height) < 0) {
-		weston_log("Cannot configure output \"%s\" using weston_rdp_output_api.\n",
-			   output->name);
-		return -1;
-	}
-
-	return 0;
-}
-
 static void
 weston_rdp_backend_config_init(struct weston_rdp_backend_config *config)
 {
@@ -3124,6 +3090,83 @@ weston_rdp_backend_config_init(struct weston_rdp_backend_config *config)
 	config->refresh_rate = RDP_DEFAULT_FREQ;
 }
 
+static void
+rdp_handle_layout(struct weston_compositor *ec)
+{
+	struct wet_compositor *wc = to_wet_compositor(ec);
+	struct wet_output_config *parsed_options = wc->parsed_options;
+	const struct weston_rdp_output_api *api = weston_rdp_output_get_api(ec);
+	struct weston_rdp_monitor config;
+	struct weston_head *head = NULL;
+	int width;
+	int height;
+	int scale = 1;
+
+	while ((head = weston_compositor_iterate_heads(ec, head))) {
+		struct weston_output *output = head->output;
+		struct weston_mode new_mode = {};
+
+		assert(output);
+
+		api->head_get_monitor(head, &config);
+
+		width = config.width;
+		height = config.height;
+		scale = config.desktop_scale / 100;
+
+		/* If these are invalid, the backend is expecting
+		 * us to provide defaults.
+		 */
+		width = width ? width : parsed_options->width;
+		height = height ? height : parsed_options->height;
+		scale = scale ? scale : parsed_options->scale;
+
+		/* Fallback to 640 x 480 if we have nothing to use */
+		width = width ? width : 640;
+		height = height ? height : 480;
+		scale = scale ? scale : 1;
+
+		new_mode.width = width;
+		new_mode.height = height;
+		api->output_set_mode(output, &new_mode);
+
+		weston_output_set_scale(output, scale);
+		weston_output_set_transform(output,
+					    WL_OUTPUT_TRANSFORM_NORMAL);
+		weston_output_move(output, config.x, config.y);
+	}
+}
+
+static void
+rdp_heads_changed(struct wl_listener *listener, void *arg)
+{
+	struct weston_compositor *compositor = arg;
+	struct wet_compositor *wet = to_wet_compositor(compositor);
+	struct weston_head *head = NULL;
+
+	while ((head = weston_compositor_iterate_heads(compositor, head))) {
+		if (head->output)
+			continue;
+
+		struct weston_output *out;
+
+		out = weston_compositor_create_output(compositor,
+						      head, head->name);
+
+		wet_head_tracker_create(wet, head);
+		weston_output_attach_head(out, head);
+	}
+
+	rdp_handle_layout(compositor);
+
+	while ((head = weston_compositor_iterate_heads(compositor, head))) {
+		if (!head->output->enabled)
+			weston_output_enable(head->output);
+
+		weston_head_reset_device_changed(head);
+	}
+}
+
 static int
 load_rdp_backend(struct weston_compositor *c,
 		int *argc, char *argv[], struct weston_config *wc,
@@ -3133,8 +3176,9 @@ load_rdp_backend(struct weston_compositor *c,
 	struct weston_config_section *section;
 	int ret = 0;
 	bool no_remotefx_codec = false;
-
 	struct wet_output_config *parsed_options = wet_init_parsed_options(c);
+	struct wet_compositor *wet = to_wet_compositor(c);
+
 	if (!parsed_options)
 		return -1;
 
@@ -3151,6 +3195,7 @@ load_rdp_backend(struct weston_compositor *c,
 		{ WESTON_OPTION_STRING,  "rdp4-key", 0, &config.rdp_key },
 		{ WESTON_OPTION_STRING,  "rdp-tls-cert", 0, &config.server_cert },
 		{ WESTON_OPTION_STRING,  "rdp-tls-key", 0, &config.server_key },
+		{ WESTON_OPTION_INTEGER, "scale", 0, &parsed_options->scale },
 		{ WESTON_OPTION_BOOLEAN, "force-no-compression", 0, &config.force_no_compression },
 		{ WESTON_OPTION_BOOLEAN, "no-remotefx-codec", 0, &no_remotefx_codec },
 	};
@@ -3159,11 +3204,14 @@ load_rdp_backend(struct weston_compositor *c,
 	config.remotefx_codec = !no_remotefx_codec;
 	config.renderer = renderer;
 
-	wet_set_simple_head_configurator(c, rdp_backend_output_configure);
 	section = weston_config_get_section(wc, "rdp", NULL, NULL);
 	weston_config_section_get_int(section, "refresh-rate",
 				      &config.refresh_rate,
 				      RDP_DEFAULT_FREQ);
+
+	wet->heads_changed_listener.notify = rdp_heads_changed;
+	weston_compositor_add_heads_changed_listener(c,
+						     &wet->heads_changed_listener);
 
 	ret = weston_compositor_load_backend(c, WESTON_BACKEND_RDP,
 					     &config.base);

include/libweston/backend-rdp.h
@@ -33,16 +33,26 @@ extern "C" {
 #include <libweston/libweston.h>
 #include <libweston/plugin-registry.h>
 
-#define WESTON_RDP_OUTPUT_API_NAME "weston_rdp_output_api_v1"
+#define WESTON_RDP_OUTPUT_API_NAME "weston_rdp_output_api_v2"
 #define RDP_DEFAULT_FREQ 60
 
+struct weston_rdp_monitor {
+	int32_t x;
+	int32_t y;
+	int32_t width;
+	int32_t height;
+	uint32_t desktop_scale;
+};
+
 struct weston_rdp_output_api {
-	/** Initialize a RDP output with specified width and height.
-	 *
-	 * Returns 0 on success, -1 on failure.
+	/** Get config from RDP client when connected
 	 */
-	int (*output_set_size)(struct weston_output *output,
-			       int width, int height);
+	void (*head_get_monitor)(struct weston_head *head,
+				 struct weston_rdp_monitor *monitor);
+
+	/** Set mode for an output */
+	void (*output_set_mode)(struct weston_output *base,
+				struct weston_mode *mode);
 };
 
 static inline const struct weston_rdp_output_api *

libweston/backend-rdp/meson.build
@@ -28,6 +28,7 @@ deps_rdp = [
 srcs_rdp = [
         'rdp.c',
         'rdpclip.c',
+	'rdpdisp.c',
         'rdputil.c',
 ]
 

commit 20d8963d3cffceb4f815347325206c143f8c65a7

backend-rdp: fix for heterogeneous outputs
Fix the RDP backend to handle to_rdp_head() returning NULL.
In practice, this should only happen once multi-backend support
is enabled.

Fixes: 70b03b292805 ("rdp: Add preliminary rdp multihead support")
Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

libweston/backend-rdp/rdpdisp.c
@@ -132,7 +132,7 @@ match_heads(struct rdp_backend *rdp, rdpMonitor *config, uint32_t count,
 
 	wl_list_for_each(iter, &rdp->compositor->head_list, compositor_link) {
 		current = to_rdp_head(iter);
-		if (current->matched)
+		if (!current || current->matched)
 			continue;
 
 		for (i = 0; i < count; i++) {
@@ -165,6 +165,9 @@ disp_layout_change(freerdp_peer *client, rdpMonitor *config, UINT32 monitorCount
 	/* Prune heads that were never enabled, and flag heads as unmatched  */
 	wl_list_for_each_safe(iter, tmp, &b->compositor->head_list, compositor_link) {
 		current = to_rdp_head(iter);
+		if (!current)
+			continue;
+
 		if (!iter->output) {
 			rdp_head_destroy(iter);
 			continue;
@@ -192,6 +195,9 @@ disp_layout_change(freerdp_peer *client, rdpMonitor *config, UINT32 monitorCount
 	/* Destroy any heads we won't be using */
 	wl_list_for_each_safe(iter, tmp, &b->compositor->head_list, compositor_link) {
 		current = to_rdp_head(iter);
+		if (!current)
+			continue;
+
 		if (!current->matched)
 			rdp_head_destroy(iter);
 	}
@@ -330,7 +336,8 @@ to_weston_coordinate(RdpPeerContext *peerContext, int32_t *x, int32_t *y)
 	wl_list_for_each(head_iter, &b->compositor->head_list, compositor_link) {
 		struct rdp_head *head = to_rdp_head(head_iter);
 
-		assert(head);
+		if (!head)
+			continue;
 
 		if (rdp_head_contains(head, sx, sy)) {
 			struct weston_output *output = head->base.output;