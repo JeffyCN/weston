JeffyCN/weston

commit b6423e59d9116d140e33e925d6dd9bf8324188a7

libweston: Add assert for valid confine region in maybe_warp_confined_pointer()
Signed-off-by: Sergio Gómez <sergio.g.delreal@gmail.com>

libweston/input.c
@@ -4730,6 +4730,7 @@ maybe_warp_confined_pointer(struct weston_pointer_constraint *constraint)
 		pixman_region32_intersect(&confine_region,
 					  &constraint->view->surface->input,
 					  &constraint->region);
+		assert(!pixman_region32_selfcheck(&confine_region));
 		region_to_outline(&confine_region, &borders);
 		pixman_region32_fini(&confine_region);
 

commit 1ed88f60c0125988cf1d952f0dabf568bfd82a13

libweston/input: Fix assert for valid confine region
We need only check that the region is not empty. If either the input region or
the constraint region have degenerate extents, the intersection from the
previous instruction will set confine_region->data to pixman_region_empty_data.

Fixes: b6423e59

Signed-off-by: Sergio Gómez <sergio.g.delreal@gmail.com>

libweston/input.c
@@ -4730,7 +4730,7 @@ maybe_warp_confined_pointer(struct weston_pointer_constraint *constraint)
 		pixman_region32_intersect(&confine_region,
 					  &constraint->view->surface->input,
 					  &constraint->region);
-		assert(!pixman_region32_selfcheck(&confine_region));
+		assert(pixman_region32_not_empty(&confine_region));
 		region_to_outline(&confine_region, &borders);
 		pixman_region32_fini(&confine_region);
 