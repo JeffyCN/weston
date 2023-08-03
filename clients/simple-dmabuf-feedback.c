1ffd387d0ef74837c639e5df4b15e7d7e589eb65
Tomohito Esaki <etom@igel.co.jp>
2023-08-03 17:21:10 +0900

    clients/simple-dmabuf-feedback: Drop support libgbm older than 21.1.1
    Since simple-dmabuf-feedback requires libgbm 21.1.1, there is no need to
    support the old libgbm which does not support modifiers.
    
    Signed-off-by: Tomohito Esaki <etom@igel.co.jp>

--- clients/simple-dmabuf-feedback.c
+++ clients/simple-dmabuf-feedback.c
@@ -546,7 +546,6 @@ create_dmabuf_buffer(struct window *window, struct buffer *buf, uint32_t width,
 	buf->height = height;
 	buf->format = format;
 
-#ifdef HAVE_GBM_MODIFIERS
 	if (count_modifiers > 0) {
 #ifdef HAVE_GBM_BO_CREATE_WITH_MODIFIERS2
 		buf->bo = gbm_bo_create_with_modifiers2(display->gbm_device,
@@ -563,7 +562,6 @@ create_dmabuf_buffer(struct window *window, struct buffer *buf, uint32_t width,
 		if (buf->bo)
 			buf->modifier = gbm_bo_get_modifier(buf->bo);
 	}
-#endif
 
 	if (!buf->bo) {
 		buf->bo = gbm_bo_create(display->gbm_device, buf->width,
