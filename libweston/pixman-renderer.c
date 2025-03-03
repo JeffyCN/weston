de53f6e87864eee20fa541c68702539d777a013d
Jeffy Chen <jeffy.chen@rock-chips.com>
2025-03-03 12:20:21 +0800

    libweston: bindings: Disable extra bindings by default
    Set env WESTON_MOD_BINDING WESTON_KEY_BINDING WESTON_DEBUG_BINDING to
    enable.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libweston/pixman-renderer.c
+++ libweston/pixman-renderer.c
@@ -1284,7 +1284,9 @@ pixman_renderer_destroy(struct weston_compositor *ec)
 #endif
 
 	wl_signal_emit(&pr->destroy_signal, pr);
-	weston_binding_destroy(pr->debug_binding);
+
+	if (pr->debug_binding)
+		weston_binding_destroy(pr->debug_binding);
 
 	weston_drm_format_array_fini(&pr->supported_formats);
 
