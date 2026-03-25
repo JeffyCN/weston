7e4d336e9fae10c4f50aa6cbe79ba5ffffcc6ee3
Jeffy Chen <jeffy.chen@rock-chips.com>
2026-03-25 15:46:59 +0800

    backend-drm: Add debounce for hotplug updates
    Introduces a debounce mechanism for DRM output hotplug events to
    prevent glitches caused by rapid successive updates.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libweston/backend-drm/drm-internal.h
+++ libweston/backend-drm/drm-internal.h
@@ -129,6 +129,8 @@
 
 #define MAX_CLONED_CONNECTORS 4
 
+/* Minimum interval between hotplug update requests (ms) to avoid glitches */
+#define DRM_HOTPLUG_DEBOUNCE_MS	1000
 
 /**
  * Represents the values of an enum-type KMS property
@@ -287,6 +289,13 @@ struct drm_backend {
 
 	/* True if we need a workaround for some very old kernels */
 	bool stale_timestamp_workaround;
+
+	/* Timer for debouncing hotplug events */
+	struct wl_event_source *hotplug_update_timer;
+	/* Flag for pending debounced update */
+	bool pending_hotplug_update;
+	/* Timestamp of last processed update */
+	int64_t last_hotplug_update_ms;
 };
 
 struct drm_mode {
