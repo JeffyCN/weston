dc7c987a52f52ade6d958cab37aed0e47dbf45d6
colorhacker <2863515010@qq.com>
2026-06-12 23:59:31 +0800

    Add debian packaging directory

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/weston/issues
+# Bug-Submit: https://github.com/<user>/weston/issues/new
+# Changelog: https://github.com/<user>/weston/blob/master/CHANGES
+# Documentation: https://github.com/<user>/weston/wiki
+# Repository-Browse: https://github.com/<user>/weston
+# Repository: https://github.com/<user>/weston.git
